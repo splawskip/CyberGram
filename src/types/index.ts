2b833a3fcad6b0430c856300a80342acf52de326
Przemysław Spławski <psplawski.dev@gmail.com>
2023-11-10 17:13:53 +0100

    Add new type

--- src/types/index.ts
+++ src/types/index.ts
@@ -46,3 +46,12 @@ export type INewUser = {
   username: string;
   password: string;
 };
+
+export type IContextType = {
+  user: IUser;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
+};
