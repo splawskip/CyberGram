fd37f295773e08dd2f3bd6ebbadce8d5a3a4bcb7
Przemysław Spławski <psplawski.dev@gmail.com>
2023-11-10 17:13:29 +0100

    Add tanstack query config files

---
+++ src/lib/react-query/queriesAndMutations.ts
@@ -0,0 +1,13 @@
+import {
+  useMutation,
+} from '@tanstack/react-query';
+import { createUserAccount, signInAccount } from '../appwrite/api';
+import { INewUser } from '@/types';
+
+export const useCreateUserAccount = () => useMutation({
+  mutationFn: (user: INewUser) => createUserAccount(user),
+});
+
+export const useSignInAccount = () => useMutation({
+  mutationFn: (user: { email:string, password: string }) => signInAccount(user),
+});
