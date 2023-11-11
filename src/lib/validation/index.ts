699e5d7ef291bd4ffb99aa6ba0aa71b8a04b42b4
Przemysław Spławski <psplawski.dev@gmail.com>
2023-11-11 21:34:23 +0100

    Add validation for post create form

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -11,3 +11,10 @@ export const SignInValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: 'Password must be at least 8 characters long.' }),
 });
+
+export const PostUploadValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
