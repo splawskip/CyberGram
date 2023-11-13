ebdacd24ab905b3608373f64d7ccb1926f3423fc
Przemysław Spławski <psplawski.dev@gmail.com>
2023-11-13 17:15:04 +0100

    Create PostCard and PostStats components

---
+++ src/components/shared/PostCard.tsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import { PostCardProps } from '@/types';
+import { getRelativeTime } from '@/lib/utils';
+import { useUserContext } from '@/context/AuthContext';
+import PostStats from './PostStats';
+
+function PostCard({ post }:PostCardProps) {
+  const { user } = useUserContext();
+
+  return (
+    <div className="post-card">
+      <div className="flex-between flex-wrap">
+        <div className="flex items-center gap-3">
+          <Link to={`/profile/${post.creator.$id}`}>
+            <img src={post?.creator?.imageUrl ?? '/assets/icons/profile-placeholder.svg'} className="rounded-full w-12 lg:h-12" alt="Creator avatar" />
+          </Link>
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bold text-light-1">{post.creator.name}</p>
+            <div className="flex-start gap-2 text-light-3">
+              <p className="subtle-semibold lg:small-regular">{getRelativeTime(post.$createdAt)}</p>
+              -
+              <p className="subtle-semibold lg:small-regular">{post.location}</p>
+            </div>
+          </div>
+        </div>
+
+        {user.id !== post.creator.$id && (
+          <Link to={`/update-post/${post.$id}`}>
+            <img src="/assets/icons/edit.svg" alt="Edit icon" width={20} height={20} />
+          </Link>
+        )}
+
+        <Link to={`/posts/${post.$id}`} className="w-full">
+          <div className="small-medium lg:base-medium py-5">
+            <p>{post.caption}</p>
+            <ul className="flex gap-1 mt-2">
+              {post.tags.map((tag:string) => (
+                <li key={tag} className="text-light-3">
+                  #
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <img loading="lazy" src={post.imageUrl ?? '/assets/icons/profile-placeholder.svg'} alt="Post content" className="post-card_img" />
+        </Link>
+      </div>
+      <PostStats post={post} userId={user.id} />
+    </div>
+  );
+}
+
+export default PostCard;
---
+++ src/components/shared/PostStats.tsx
@@ -0,0 +1,74 @@
+import { Models } from 'appwrite';
+import React from 'react';
+import {
+  useDeleteSavePost, useGetCurrentUser, useLikePost, useSavePost,
+} from '@/lib/react-query/queriesAndMutations';
+import { checkIsLiked } from '@/lib/utils';
+import Loader from './Loader';
+
+type PostStatsProps = {
+  post: Models.Document,
+  userId: string,
+};
+function PostStats({ post, userId } : PostStatsProps) {
+  const likesList = post.likes.map((user:Models.Document) => user.$id);
+
+  const [likes, setLikes] = React.useState(likesList);
+  const [isSaved, setIsSaved] = React.useState(false);
+
+  const { mutate: likePost } = useLikePost();
+  const { mutate: savePost, isPending: isSaving } = useSavePost();
+  const { mutate: deleteSavedPost, isPending: isDeleting } = useDeleteSavePost();
+
+  const { data: currentUser } = useGetCurrentUser();
+
+  const savedPost = currentUser?.save.find(
+    (record:Models.Document) => record.post.$id === post.$id,
+  );
+
+  React.useEffect(() => {
+    setIsSaved(!!savedPost);
+  }, [currentUser, savedPost]);
+
+  const handleLikePost = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    let newLikes = [...likes];
+    if (newLikes.includes(userId)) {
+      newLikes = newLikes.filter((id) => id !== userId);
+    } else {
+      newLikes.push(userId);
+    }
+    setLikes(newLikes);
+    likePost({ postId: post.$id, likesArray: newLikes });
+  };
+
+  const handleSavePost = (event: React.MouseEvent) => {
+    event.stopPropagation();
+
+    if (savedPost) {
+      setIsSaved(false);
+      deleteSavedPost(savedPost.$id);
+    } else {
+      setIsSaved(true);
+      savePost({ postId: post.$id, userId: currentUser?.$id });
+    }
+  };
+
+  return (
+    <div className="flex justify-between items-center z-20">
+      <div className="flex gap-2 mr-5">
+        <button type="button" onClick={handleLikePost}>
+          {isSaving || isDeleting ? <Loader /> : <img src={`/assets/icons/like${checkIsLiked(likes, userId) ? 'd' : ''}.svg`} alt="Like icon" width={20} height={20} className="cursor-pointer" />}
+        </button>
+        <p className="small-medium lg:base-medium">{likes.length}</p>
+      </div>
+      <div className="flex gap-2">
+        <button type="button" onClick={handleSavePost}>
+          {isSaving || isDeleting ? <Loader /> : <img src={`/assets/icons/save${isSaved ? 'd' : ''}.svg`} alt="Save icon" width={20} height={20} className="cursor-pointer" />}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PostStats;
