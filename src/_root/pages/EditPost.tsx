e7aa9bf438103c60f65afc24c082fc76d6f5e150
Przemysław Spławski <psplawski.dev@gmail.com>
2023-11-13 20:23:52 +0100

    Create view for Post Edit

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,5 +1,24 @@
+import { useParams } from 'react-router-dom';
+import { Loader } from 'lucide-react';
+import PostForm from '@/components/forms/PostForm';
+import { useGetPostById } from '@/lib/react-query/queriesAndMutations';
+
 function EditPost() {
-  return (<div>EditPost</div>);
+  const { id } = useParams();
+  const { data: post, isPending } = useGetPostById(id ?? '');
+
+  if (isPending) return <Loader />;
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
+          <img src="/assets/icons/add-post.svg" width={36} height={36} alt="Add post icon" />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
+        </div>
+        <PostForm action="Update" post={post} />
+      </div>
+    </div>
+  );
 }
 
 export default EditPost;
