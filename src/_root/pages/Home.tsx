aee30df7cdb6f1af16caa397160401fb42603253
Przemysław Spławski <psplawski.dev@gmail.com>
2023-11-13 17:15:22 +0100

    Display posts on the homepage

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,6 +1,26 @@
+import { Models } from 'appwrite';
+import Loader from '@/components/shared/Loader';
+import { useGetRecentPosts } from '@/lib/react-query/queriesAndMutations';
+import PostCard from '@/components/shared/PostCard';
+
 function Home() {
+  const { data: posts, isPending: isPostLoading } = useGetRecentPosts();
+  // Build component.
   return (
-    <div>Home</div>
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2 text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? (<Loader />) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post:Models.Document) => (
+                <PostCard key={post.$id} post={post} />
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
